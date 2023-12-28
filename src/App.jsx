543059615cccc9cec79e8f016ead16cf8763efec
hisbanshiraz <hisbanbardai@gmail.com>
2023-12-28 13:57:23 -0700

    Add an activePlayer state and lift it up to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import GameBoard from "./components/Gameboard";
 import Player from "./components/Player";
 
 function App() {
+  //Below what we are doing is called lifting the state up
+  //We are handling the state of the active player because we want to highlight the active player. But we are defining the state here and not in the Player component because the active player state should get updated when a square/button is clicked. So that means our Game component also needs the active player state because we will also display "X" or "O" in gameboard on the basis of active player. That means both Player and Gameboard needs access to active player state and that's why we moved the state to the common ancestor of both child components i.e. App component.
+  //NOTE: we are handling active player on the basis of symbols "X" and "O"
+  const [activePlayer, setactivePlayer] = useState("X");
+
+  function handleActivePlayer() {
+    setactivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName={"Player 1"} symbol={"X"} />
-          <Player initialName={"Player 2"} symbol={"0"} />
+        <ol id="players" className="highlight-player">
+          <Player
+            isActive={activePlayer === "X"}
+            initialName={"Player 1"}
+            symbol={"X"}
+          />
+          <Player
+            isActive={activePlayer === "O"}
+            initialName={"Player 2"}
+            symbol={"O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          activePlayerSymbol={activePlayer}
+          switchPlayer={handleActivePlayer}
+        />
       </div>
     </main>
   );
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -8,7 +8,7 @@ const initialGameboard = [
 
 //we have created the above initialGameboard because we know for tic tac toe game we need a 3x3 grid and this info is static (it will not change). Then we use the above array to create our html structure below i.e. the outer array is the ol element which contains three li elements and each li element contains a child ol element and this child ol element contains three li elements. The only thing dynamic will be the values inside button elements
 
-export default function GameBoard() {
+export default function GameBoard({ activePlayerSymbol, switchPlayer }) {
   const [gameBoard, setGameboard] = useState(initialGameboard);
 
   function handleClickSquare(rowIndex, colIndex) {
@@ -21,9 +21,11 @@ export default function GameBoard() {
       const updatedGameBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ];
-      updatedGameBoard[rowIndex][colIndex] = "X";
+      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedGameBoard;
     });
+
+    switchPlayer();
   }
 
   return (
@@ -36,6 +38,7 @@ export default function GameBoard() {
                 return (
                   <li key={colIndex}>
                     <button
+                      //sending information which square was clicked
                       onClick={() => handleClickSquare(rowIndex, colIndex)}
                     >
                       {col}
