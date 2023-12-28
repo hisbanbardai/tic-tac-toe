8abefb6579c9bb66cae25b29f8e9b020ecf8b8e6
hisbanshiraz <hisbanbardai@gmail.com>
2023-12-28 15:06:42 -0700

    Add a log component and gameTurns state and lift it up to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
 import GameBoard from "./components/Gameboard";
 import Player from "./components/Player";
+import Logs from "./components/Log";
 
 function App() {
-  //Below what we are doing is called lifting the state up
+  const [activePlayer, setactivePlayer] = useState("X");
+  //Above what we are doing is called lifting the state up
   //We are handling the state of the active player because we want to highlight the active player. But we are defining the state here and not in the Player component because the active player state should get updated when a square/button is clicked. So that means our Game component also needs the active player state because we will also display "X" or "O" in gameboard on the basis of active player. That means both Player and Gameboard needs access to active player state and that's why we moved the state to the common ancestor of both child components i.e. App component.
   //NOTE: we are handling active player on the basis of symbols "X" and "O"
-  const [activePlayer, setactivePlayer] = useState("X");
 
-  function handleActivePlayer() {
+  const [gameTurns, setGameTurns] = useState([]);
+  //above we are also lifting the state up because the gameTurns state is going to be used by Gameboard and Log components.
+
+  function handleSelectSquare(rowIndex, colIndex) {
     setactivePlayer((currActivePlayer) =>
       currActivePlayer === "X" ? "O" : "X"
     );
+
+    setGameTurns((prevGameTurns) => {
+      //below we are updating the state of an object i.e. array, immutably
+      const updatedGameTurns = [
+        { square: { rowIndex, colIndex }, player: activePlayer },
+        ...prevGameTurns,
+      ];
+    });
   }
 
   return (
@@ -31,9 +43,10 @@ function App() {
         </ol>
         <GameBoard
           activePlayerSymbol={activePlayer}
-          switchPlayer={handleActivePlayer}
+          clickSquare={handleSelectSquare}
         />
       </div>
+      <Logs />
     </main>
   );
 }
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -8,25 +8,25 @@ const initialGameboard = [
 
 //we have created the above initialGameboard because we know for tic tac toe game we need a 3x3 grid and this info is static (it will not change). Then we use the above array to create our html structure below i.e. the outer array is the ol element which contains three li elements and each li element contains a child ol element and this child ol element contains three li elements. The only thing dynamic will be the values inside button elements
 
-export default function GameBoard({ activePlayerSymbol, switchPlayer }) {
-  const [gameBoard, setGameboard] = useState(initialGameboard);
+export default function GameBoard({ activePlayerSymbol, clickSquare }) {
+  // const [gameBoard, setGameboard] = useState(initialGameboard);
 
-  function handleClickSquare(rowIndex, colIndex) {
-    setGameboard((prevGameBoard) => {
-      //we should not do the below because objects are mutable and the below line of code will directly change the object in memory and as we know React schedules state updates, so the object in memory will be changed before React updates the state and this can cause issues. Instead we should create a deep copy of the object using spread operator
-      // prevGameBoard[rowIndex][colIndex] = "X";
-      // return prevGameBoard;
+  // function handleClickSquare(rowIndex, colIndex) {
+  //   setGameboard((prevGameBoard) => {
+  //     //we should not do the below because objects are mutable and the below line of code will directly change the object in memory and as we know React schedules state updates, so the object in memory will be changed before React updates the state and this can cause issues. Instead we should create a deep copy of the object using spread operator
+  //     // prevGameBoard[rowIndex][colIndex] = "X";
+  //     // return prevGameBoard;
 
-      //we did the below because spread operator only creates a shallow copy of the object which means that if we modify any nested element then this change will also be affected to the original object. Therefore we need to deeply copy the object i.e. inner Array
-      const updatedGameBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedGameBoard;
-    });
+  //     //we did the below because spread operator only creates a shallow copy of the object which means that if we modify any nested element then this change will also be affected to the original object. Therefore we need to deeply copy the object i.e. inner Array
+  //     const updatedGameBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedGameBoard;
+  //   });
 
-    switchPlayer();
-  }
+  //   switchPlayer();
+  // }
 
   return (
     <ol id="game-board">
@@ -39,7 +39,7 @@ export default function GameBoard({ activePlayerSymbol, switchPlayer }) {
                   <li key={colIndex}>
                     <button
                       //sending information which square was clicked
-                      onClick={() => handleClickSquare(rowIndex, colIndex)}
+                      onClick={() => clickSquare(rowIndex, colIndex)}
                     >
                       {col}
                     </button>
