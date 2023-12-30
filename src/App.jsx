d3a44e146dc253da68a9adf192ba790a45a5c328
hisbanshiraz <hisbanbardai@gmail.com>
2023-12-30 13:27:43 -0700

    Removed activePlayer state and derived it from gameTurns state

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,34 @@ import GameBoard from "./components/Gameboard";
 import Player from "./components/Player";
 import Logs from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let activePlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    activePlayer = "O";
+  }
+
+  return activePlayer;
+}
+
 function App() {
-  const [activePlayer, setactivePlayer] = useState("X");
+  // const [activePlayer, setactivePlayer] = useState("X");
   //Above what we are doing is called lifting the state up
   //We are handling the state of the active player because we want to highlight the active player. But we are defining the state here and not in the Player component because the active player state should get updated when a square/button is clicked. So that means our Game component also needs the active player state because we will also display "X" or "O" in gameboard on the basis of active player. That means both Player and Gameboard needs access to active player state and that's why we moved the state to the common ancestor of both child components i.e. App component.
   //NOTE: we are handling active player on the basis of symbols "X" and "O"
 
+  //If we notice one thing above, it is that we do not need to have a separate state for activePlayer. We can derive its state from the gameTurns state. game turn is an object that contains information about the active player and the clicked square co-ordinates.
+
   const [gameTurns, setGameTurns] = useState([]);
   //above we are also lifting the state up because the gameTurns state is going to be used by Gameboard and Log components.
 
+  //DERIVING ACTIVE PLAYER STATE FROM GAMETURNS instead of managing its own separate state
+  const activePlayer = deriveActivePlayer(gameTurns);
+
   function handleSelectSquare(rowIndex, colIndex) {
-    setactivePlayer((currActivePlayer) =>
-      currActivePlayer === "X" ? "O" : "X"
-    );
+    // setactivePlayer((currActivePlayer) =>
+    //   currActivePlayer === "X" ? "O" : "X"
+    // );
 
     setGameTurns((prevGameTurns) => {
       //below we are updating the state of an object i.e. array, immutably
@@ -26,11 +41,10 @@ function App() {
       // ];
 
       //To resolve the above mentioned issue, here we are assuming activePLayer as "X" and then comparing it with the recent gameTurn's activePlayer. We have used prevGameTurns[0] because we are saving the recent gameTurn at first place. We have also catered the case if gameTurns array is empty which would be the case at first turn.
-      let currentPlayer = "X";
 
-      if (prevGameTurns.length > 0 && prevGameTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      //DERIVING CURRENT PLAYER STATE FROM PREVGAMETURNS
+      const currentPlayer = deriveActivePlayer(prevGameTurns);
+
       const updatedGameTurns = [
         { square: { rowIndex, colIndex }, player: currentPlayer },
         ...prevGameTurns,
