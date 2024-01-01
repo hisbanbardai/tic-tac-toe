c9f1d9922c6448fc1a1d78211a6275229ee9b392
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-01 13:56:01 -0700

    Refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Logs from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver";
 
-const initialGameboard = [
+const INITIAL_GAMEBOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
+
+const PLAYERS = { X: "Player 1", O: "Player 2" };
 
 function deriveActivePlayer(gameTurns) {
   let activePlayer = "X";
@@ -21,38 +23,11 @@ function deriveActivePlayer(gameTurns) {
   return activePlayer;
 }
 
-function App() {
-  // const [activePlayer, setactivePlayer] = useState("X");
-  //Above what we are doing is called lifting the state up
-  //We are handling the state of the active player because we want to highlight the active player. But we are defining the state here and not in the Player component because the active player state should get updated when a square/button is clicked. So that means our Game component also needs the active player state because we will also display "X" or "O" in gameboard on the basis of active player. That means both Player and Gameboard needs access to active player state and that's why we moved the state to the common ancestor of both child components i.e. App component.
-  //NOTE: we are handling active player on the basis of symbols "X" and "O"
-
-  //If we notice one thing above, it is that we do not need to have a separate state for activePlayer. We can derive its state from the gameTurns state. game turn is an object that contains information about the active player and the clicked square co-ordinates.
-
-  const [gameTurns, setGameTurns] = useState([]);
-  //above we are also lifting the state up because the gameTurns state is going to be used by Gameboard and Log components.
-
-  const [playerName, setPlayerName] = useState({
-    X: "Player 1",
-    O: "Player 2",
-  });
-  //above we are setting a separate players names state and not lifting the player name state from Player component because in Player component we are update the Player state everytime user input is changed. So if we lift it up to the App component then on every input change whole App component will be rendered
-  function handlePlayerName(symbol, playerName) {
-    setPlayerName((prevPlayersName) => {
-      return {
-        ...prevPlayersName,
-        [symbol]: playerName,
-      };
-    });
-  }
-
-  //DERIVING ACTIVE PLAYER STATE FROM GAMETURNS instead of managing its own separate state
-  const activePlayer = deriveActivePlayer(gameTurns);
-
+function deriveGameBoard(gameTurns) {
   //we should not do the below because of the immutability rule i.e. objects cannot be copied just by assignment operator. They will always refer to the same object in memory
   // const gameBoard = initialGameboard;
 
-  const gameBoard = [...initialGameboard.map((array) => [...array])];
+  const gameBoard = [...INITIAL_GAMEBOARD.map((array) => [...array])];
   for (const turn of gameTurns) {
     //object destructuring below
     const { square, player } = turn;
@@ -60,6 +35,11 @@ function App() {
 
     gameBoard[rowIndex][colIndex] = player;
   }
+
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, playerName) {
   //Below we are going through every winning combination each time App component gets re-rendered to see if we have a winner. But to get the values off of the gameboard we need access to it. That is why we will lift the game board from Gameboard component to App component
   let winner;
 
@@ -76,6 +56,38 @@ function App() {
       winner = playerName[firstSymbol];
     }
   }
+
+  return winner;
+}
+
+function App() {
+  // const [activePlayer, setactivePlayer] = useState("X");
+  //Above what we are doing is called lifting the state up
+  //We are handling the state of the active player because we want to highlight the active player. But we are defining the state here and not in the Player component because the active player state should get updated when a square/button is clicked. So that means our Game component also needs the active player state because we will also display "X" or "O" in gameboard on the basis of active player. That means both Player and Gameboard needs access to active player state and that's why we moved the state to the common ancestor of both child components i.e. App component.
+  //NOTE: we are handling active player on the basis of symbols "X" and "O"
+
+  //If we notice one thing above, it is that we do not need to have a separate state for activePlayer. We can derive its state from the gameTurns state. game turn is an object that contains information about the active player and the clicked square co-ordinates.
+
+  const [gameTurns, setGameTurns] = useState([]);
+  //above we are also lifting the state up because the gameTurns state is going to be used by Gameboard and Log components.
+
+  const [playerName, setPlayerName] = useState(PLAYERS);
+  //above we are setting a separate players names state and not lifting the player name state from Player component because in Player component we are update the Player state everytime user input is changed. So if we lift it up to the App component then on every input change whole App component will be rendered
+  function handlePlayerName(symbol, playerName) {
+    setPlayerName((prevPlayersName) => {
+      return {
+        ...prevPlayersName,
+        [symbol]: playerName,
+      };
+    });
+  }
+
+  //DERIVING ACTIVE PLAYER STATE FROM GAMETURNS instead of managing its own separate state
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  const gameBoard = deriveGameBoard(gameTurns);
+
+  const winner = deriveWinner(gameBoard, playerName);
 
   //as we know that there can be total 9 turns so below we are checking if total turns are 9 and we do not have a winner then set gameOver as true
   const gameOver = gameTurns.length === 9 && !winner;
@@ -117,13 +129,13 @@ function App() {
         <ol id="players" className="highlight-player">
           <Player
             isActive={activePlayer === "X"}
-            initialName={"Player 1"}
+            initialName={PLAYERS.X}
             symbol={"X"}
             onPlayerNameChange={handlePlayerName}
           />
           <Player
             isActive={activePlayer === "O"}
-            initialName={"Player 2"}
+            initialName={PLAYERS.O}
             symbol={"O"}
             onPlayerNameChange={handlePlayerName}
           />
